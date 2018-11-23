abd7a34cfdcf7d85efcceb9ad2b183f696920912
YZKA1 <estoran@163.com>
2018-11-23 11:31:12 +0800

    第二次

--- myprogram/src/main.js
+++ myprogram/src/main.js
@@ -1,6 +1,16 @@
 // 入口文件
-
 import Vue from 'vue'
+// 导入路由的包
+import VueRouter from 'vue-router'
+// 安装路由
+Vue.use(VueRouter)
+
+// 导入 vue-resource
+
+import VueResource from 'vue-resource'
+
+// 安装 vue-resource
+Vue.use(VueResource)
 
 // 导入 MUI 的样式
 import './lib/mui/css/mui.min.css'
@@ -9,23 +19,24 @@ import './lib/mui/css/icons-extra.css'
 
 // 导入Mint-UI 中的组件
 
-import { Header } from 'mint-ui';
+import { Header,Swipe, SwipeItem } from 'mint-ui';
 
-Vue.component(Header.name, Header);
+Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
- 
+//导入自己的 router.js 路由模块
+
+import router from './router/index.js'
 
 // 导入 app 根组件
 import App from './App'
-
-
-import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
-  render: h => h(App)
 })
--- myprogram/src/router/index.js
+++ myprogram/src/router/index.js
@@ -1,15 +1,25 @@
-// import Vue from 'vue'
-// import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// 导入对应的路由组件
+import homeContainer from '../page/home/'
+import memberContainer from '../page/member/index.vue'
+import searchContainer from '../page/search/index.vue'
+import shopcarContainer from '../page/shopcar/index.vue'
+Vue.use(VueRouter)
+// 创建路由对象
 
-// Vue.use(Router)
 
-// export default new Router({
-//   routes: [
-//     {
-//       path: '/',
-//       name: 'HelloWorld',
-//       component: HelloWorld
-//     }
-//   ]
-// })
+var router = new VueRouter({
+    routes: [  // 配置路由规则
+        {path: '/',redirect:'/home'},
+        {path: '/home', component: homeContainer},
+        {path: '/member', component: memberContainer},
+        {path: '/search', component: searchContainer},
+        {path: '/shopcar', component: shopcarContainer},
+    ],
+    linkActiveClass: 'mui-active' //覆盖默认路由高亮的类
+})
+
+
+// 暴露路由模块
+export default router
